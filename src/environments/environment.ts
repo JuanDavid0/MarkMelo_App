4c30fbcc7734b7016359ba6e7bac8d872c9215c6
JuanDavid0 <juanochoa20029128@gmail.com>
2024-02-26 23:19:51 -0500

    prueba de coneccion a API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  urlApiRestful: 'https://api.uptc.online',
+  sales: '/sales/',
+  users: '/users/',
+  orders: '/orders/',
+  authorization: 'c5LTA6WPbMwHhEabYu77nN9cn4VcMj'
 };
 
 /*
